21acbfead9367fb3e6722ebcd3d998db345ab897
Om Kulkarni <workybuzz0405@gmail.com>
2026-03-25 23:29:52 +0530

    Added UI

--- student-performance/app/app.py
+++ student-performance/app/app.py
@@ -17,32 +17,17 @@ columns = joblib.load(columns_path)
 def home():
     return render_template('index.html')
 
+
+
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.json
-    
-    # Convert input to array
-    features = np.zeros((1, 41))  # match training feature size
-    
-    prediction = model.predict(features)
-    
-    return jsonify({
-        'prediction': float(prediction[0])
-    })
-
-@app.route('/predict_form', methods=['POST'])
-def predict_form():
-
-    studytime = int(request.form['studytime'])
-    failures = int(request.form['failures'])
-    absences = int(request.form['absences'])
+    studytime = float(request.form['studytime'])
+    failures = float(request.form['failures'])
+    absences = float(request.form['absences'])
 
     input_df = pd.DataFrame(columns=columns)
-    
-    # initialize all values to 0
     input_df.loc[0] = 0
-   
-    # assign user inputs
+
     input_df['studytime'] = studytime
     input_df['failures'] = failures
     input_df['absences'] = absences
@@ -50,7 +35,7 @@ def predict_form():
     prediction = model.predict(input_df)
 
     return render_template('index.html',
-                           prediction_text=f"Predicted Score: {prediction[0]:.2f}")
+                           prediction_text=f"Predicted Score: {round(prediction[0],2)}")
 
 if __name__ == '__main__':
     app.run(debug=True)
